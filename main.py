8c225321ad2c0a095661b383f7eab9f1518cef4a
xAlayham <alayham.aw.1@gmail.com>
2026-09-15 01:21:40 +0100

    Add CORS into main.py for compatability with frontend

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ from fastapi import FastAPI
 from app import models
 from app.database import engine
 from app.routers import users, habits
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(habits.router)
+
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def get_api_info():
